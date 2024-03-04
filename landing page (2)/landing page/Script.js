f1f1682b73787e2c9e6c7bf952fe44f409d1e8ed
Sangram lad <131296480+s1a2n3g4@users.noreply.github.com>
2024-03-05 00:01:04 +0530

    Web projects

---
+++ landing page (2)/landing page/Script.js
@@ -0,0 +1,10 @@
+
+// gsap.from('.logo', {opacity: 0, duration: 1, delay: 2, y:10, stagger:0.2});
+// gsap.from('.navbar .nav_item', {opacity: 0, duration: 1, delay: 2.1, y:30, stagger:0.2});
+
+
+// gsap.from('.title', {opacity: 0, duration: 1, delay: 1.6, y:30, stagger:0.2 });
+// gsap.from('.description', {opacity: 0, duration: 1, delay: 1.8, y:30, stagger:0.2});
+// gsap.from('.btn', {opacity: 0, duration: 1, delay: 2.1, y:30, stagger:0.2 });
+// gsap.from('.image', {opacity: 0, duration: 1, delay: 2.6, y:30, stagger:0.2});
+
